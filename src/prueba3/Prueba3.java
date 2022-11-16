60d83a24a3391ecfbdb1e0199313c204731e8c73
downb <downb@Saiarapc>
2022-11-16 13:29:55 -0800

    Bienvenido

---
+++ src/prueba3/Prueba3.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba3;
+
+/**
+ *
+ * @author downb
+ */
+public class Prueba3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola2");    }
+    
+}
